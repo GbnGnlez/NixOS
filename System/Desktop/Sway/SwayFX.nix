6a86ce743c9d876aeb906f12dd8a1e84d13b751a
GbnGnlez <GibranN.GonzalezS@outlook.com>
2026-08-27 05:51:01 -0600

    Add SwayFX configuration for enhanced visual effects in Sway

---
+++ System/Desktop/Sway/SwayFX.nix
@@ -0,0 +1,21 @@
+# https://wiki.nixos.org/wiki/Sway#SwayFX
+
+{ pkgs, ... }:
+
+{
+  wayland.windowManager.sway = {
+    enable = true;
+    package = pkgs.swayfx;
+
+    # Needed to build without errors.
+    checkConfig = false;
+
+    # SwayFX options must be configured through extraConfig.
+    extraConfig = ''
+      shadows enable
+      corner_radius 11
+      blur_radius 7
+      blur_passes 2
+    '';
+  };
+}
