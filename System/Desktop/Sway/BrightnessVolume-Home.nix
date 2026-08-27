4bc82eb96c3d927be7032d3e0fa54c64dca9224b
GbnGnlez <GibranN.GonzalezS@outlook.com>
2026-08-27 12:43:24 -0600

    Refactor Sway brightness and volume keybindings for consistency

--- System/Desktop/Sway/BrightnessVolume-Home.nix
+++ System/Desktop/Sway/BrightnessVolume-Home.nix
@@ -3,18 +3,14 @@
 { lib, ... }:
 
 {
-  wayland.windowManager.sway = {
-    config = {
-      keybindings = lib.mkOptionDefault {
-        # Brightness Controls
-        "XF86MonBrightnessDown" = "exec light -U 10";
-        "XF86MonBrightnessUp" = "exec light -A 10";
+  wayland.windowManager.sway.config.keybindings = lib.mkOptionDefault {
+    # Brightness Controls
+    "XF86MonBrightnessDown" = "exec light -U 10";
+    "XF86MonBrightnessUp" = "exec light -A 10";
 
-        # Volume Controls
-        "XF86AudioRaiseVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ +1%";
-        "XF86AudioLowerVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ -1%";
-        "XF86AudioMute" = "exec pactl set-sink-mute @DEFAULT_SINK@ toggle";
-      };
-    };
+    # Volume Controls
+    "XF86AudioRaiseVolume" = "exec wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 1%+";
+    "XF86AudioLowerVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%-";
+    "XF86AudioMute" = "exec wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
   };
 }
