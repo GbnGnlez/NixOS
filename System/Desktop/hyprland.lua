bfe7599858ff8af9335b8f23b374bc215130c0aa
GbnGnlez <GibranN.GonzalezS@outlook.com>
2026-08-27 00:28:09 -0600

    Update cursor size in Hyprland configuration to 16

--- System/Desktop/hyprland.lua
+++ System/Desktop/hyprland.lua
@@ -64,8 +64,8 @@ end)
 
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
 
-hl.env("XCURSOR_SIZE", "24")
-hl.env("HYPRCURSOR_SIZE", "24")
+hl.env("XCURSOR_SIZE", "16")
+hl.env("HYPRCURSOR_SIZE", "16")
 
 
 -----------------------
