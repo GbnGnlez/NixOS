89e8de53d77afbf6345d66241bf31ca6ee896d98
GbnGnlez <GibranN.GonzalezS@outlook.com>
2026-08-26 23:37:38 -0600

    Refactor autostart commands in Hyprland configuration for clarity

--- System/Desktop/hyprland.lua
+++ System/Desktop/hyprland.lua
@@ -51,11 +51,11 @@ local menu        = "hyprlauncher"
 -- Or execute your favorite apps at launch like this:
 --
 hl.on("hyprland.start", function () 
---   hl.exec_cmd(terminal)
---   hl.exec_cmd("nm-applet")
-     hl.exec_cmd("waybar")
---   hl.exec_cmd("waybar & hyprpaper & firefox")
--- end)
+--  hl.exec_cmd(terminal)
+--  hl.exec_cmd("nm-applet")
+    hl.exec_cmd("waybar")
+--  hl.exec_cmd("waybar & hyprpaper & firefox")
+end)
 
 
 -------------------------------
