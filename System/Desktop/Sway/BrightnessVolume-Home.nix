4bb7b8604b75e40e142e92637cf4a182fbaeb58a
GbnGnlez <GibranN.GonzalezS@outlook.com>
2026-08-27 12:44:46 -0600

    Adjust brightness and volume keybindings for improved control granularity

--- System/Desktop/Sway/BrightnessVolume-Home.nix
+++ System/Desktop/Sway/BrightnessVolume-Home.nix
@@ -5,12 +5,12 @@
 {
   wayland.windowManager.sway.config.keybindings = lib.mkOptionDefault {
     # Brightness Controls
-    "XF86MonBrightnessDown" = "exec light -U 10";
-    "XF86MonBrightnessUp" = "exec light -A 10";
+    "XF86MonBrightnessDown" = "exec light -U 5";
+    "XF86MonBrightnessUp" = "exec light -A 5";
 
     # Volume Controls
-    "XF86AudioRaiseVolume" = "exec wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 1%+";
-    "XF86AudioLowerVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%-";
+    "XF86AudioRaiseVolume" = "exec wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%+";
+    "XF86AudioLowerVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-";
     "XF86AudioMute" = "exec wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
   };
 }
