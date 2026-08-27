d8ff61122ef242e08604c71c3fd7a327c0cfa9db
GbnGnlez <GibranN.GonzalezS@outlook.com>
2026-08-26 23:38:34 -0600

    Update waybar autostart command in Hyprland configuration

--- System/Desktop/hyprland.lua
+++ System/Desktop/hyprland.lua
@@ -53,7 +53,7 @@ local menu        = "hyprlauncher"
 hl.on("hyprland.start", function () 
 --  hl.exec_cmd(terminal)
 --  hl.exec_cmd("nm-applet")
-    hl.exec_cmd("waybar")
+    hl.exec_cmd("uwsm app -- waybar")
 --  hl.exec_cmd("waybar & hyprpaper & firefox")
 end)
 
