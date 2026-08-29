6cdfb98348f8df110aab1b4c0ed1195b57343c5c
GbnGnlez <GibranN.GonzalezS@outlook.com>
2026-08-29 14:47:03 -0600

    Add WayBar settings for main bar configuration

--- System/Desktop/Niri/WayBar.nix
+++ System/Desktop/Niri/WayBar.nix
@@ -5,5 +5,18 @@
 {
   programs.waybar = {
     enable = true;
+
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "top";
+        height = 35;
+        spacing = 15;
+
+        modules-center = [
+          "clock"
+        ];
+      };
+    };
   };
 }
