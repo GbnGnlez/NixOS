897ccbbdce87283dcad7b42ae879b2cb034c21c2
GbnGnlez <GibranN.GonzalezS@outlook.com>
2026-08-29 15:03:13 -0600

    Refactor WayBar configuration to organize module definitions and improve readability

--- System/Desktop/Niri/WayBar.nix
+++ System/Desktop/Niri/WayBar.nix
@@ -14,9 +14,30 @@
         height = 35;
         margin = "15 15 0 15";
 
+        modules-left = [
+          "custom/nixos"
+        ];
+
         modules-center = [
           "clock"
         ];
+
+        modules-right = [
+          "custom/power"
+        ];
+
+        "custom/nixos" = {
+          format = "";
+          tooltip = false;
+          on-click = "fuzzel";
+        };
+
+        "custom/power" = {
+          format = "⏻";
+          tooltip = false;
+          on-click = "fuzzel --dmenu --prompt='Power: ' < <(printf 'Apagar\\nReiniciar\\nCancelar')";
+          on-click-release = "";
+        };
       };
     };
 
@@ -26,10 +47,25 @@
         border-radius: 15px;
       }
 
-      #clock { 
-        background: #1e1e2e; 
-        border-radius: 15px;  
-        padding: 0 15px; }
+      #clock {
+        background: #1e1e2e;
+        border-radius: 15px;
+        padding: 0 15px;
+      }
+
+      #custom-nixos {
+        background: #1e1e2e;
+        border-radius: 15px;
+        padding: 0 15px;
+        font-size: 20px;
+      }
+
+      #custom-power {
+        background: #1e1e2e;
+        border-radius: 15px;
+        padding: 0 15px;
+        font-size: 18px;
+      }
     '';
   };
 }
