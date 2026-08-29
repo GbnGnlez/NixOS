b752179371a31f9459083e7d2f8df64deb89ed33
GbnGnlez <GibranN.GonzalezS@outlook.com>
2026-08-29 17:14:51 -0600

    fix: add flavor and accent options to Catppuccin module configuration in Home and System

--- Home/Desktop/Catppuccin.nix
+++ Home/Desktop/Catppuccin.nix
@@ -1,8 +1,12 @@
+# https://nix.catppuccin.com/options/main/home/catppuccin/
+
 { ... }:
 
 {
   catppuccin = {
     enable = true;
     autoEnable = true;
+    flavor = "mocha";
+    accent = "blue";
   };
 }
--- System/Desktop/Catppuccin.nix
+++ System/Desktop/Catppuccin.nix
@@ -1,8 +1,12 @@
+# https://nix.catppuccin.com/options/main/nixos/catppuccin/
+
 { ... }:
 
 {
   catppuccin = {
     enable = true;
     autoEnable = true;
+    flavor = "mocha";
+    accent = "blue";
   };
 }
