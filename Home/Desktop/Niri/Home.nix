c8dca129fb8c82f16c70e6cf5a5c3985d76662be
GbnGnlez <GibranN.GonzalezS@outlook.com>
2026-08-29 20:23:18 -0600

    fix: restructure xdg.configFile for Niri configuration to include recursive option

--- Home/Desktop/Niri/Home.nix
+++ Home/Desktop/Niri/Home.nix
@@ -9,7 +9,10 @@
   ];
 
   # https://wiki.nixos.org/wiki/Niri/en#Configuration
-  xdg.configFile."niri".source = ./KDL;
+  xdg.configFile."niri" = {
+    source = ./KDL;
+    recursive = true;
+  };
 
   # https://wiki.nixos.org/wiki/Niri/en#Additional_Setup
   programs.alacritty.enable = true; # Super+T in the default setting (terminal)
