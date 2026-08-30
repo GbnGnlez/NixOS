6ac2d246251de2a626492d86383a48ca321fe6cb
GbnGnlez <GibranN.GonzalezS@outlook.com>
2026-08-29 19:54:34 -0600

    fix: update Niri configuration to specify config.kdl source and restore default configuration

--- Home/Desktop/Niri/Home.nix
+++ Home/Desktop/Niri/Home.nix
@@ -8,7 +8,7 @@
   ];
 
   # https://wiki.nixos.org/wiki/Niri/en#Configuration
-  xdg.configFile."niri".source = ./KDL;
+  xdg.configFile."niri/config.kdl".source = ./KDL/Config.kdl;
 
   # https://wiki.nixos.org/wiki/Niri/en#Additional_Setup
   programs.alacritty.enable = true; # Super+T in the default setting (terminal)
