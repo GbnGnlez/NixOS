0a73190070ff766c2fcdc8ced1ccb078a63fefe5
GbnGnlez <GibranN.GonzalezS@outlook.com>
2026-09-01 15:02:21 -0600

    fix: add battery module to WayBar configuration and clean up persistent workspaces

--- Home/Desktop/Niri/WayBar.nix
+++ Home/Desktop/Niri/WayBar.nix
@@ -34,6 +34,8 @@
           "custom/sep"
           "disk"
           "custom/sep"
+          "battery"
+          "custom/sep"
           "clock"
           "custom/sep"
           "tray"
@@ -44,10 +46,6 @@
           all-outputs = true;
           warp-on-scroll = false;
           format = "{name}";
-
-          persistent-workspaces = {
-            "*" = 9;
-          };
         };
 
         "niri/window" = {
