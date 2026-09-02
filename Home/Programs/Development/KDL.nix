f3b12c0e73c7eb9e08eec3e288a7d538f093afb2
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-09-02 09:38:24 -0600

    fix: refactor kdl-v1 to use buildVscodeMarketplaceExtension in KDL.nix

--- Home/Programs/Development/KDL.nix
+++ Home/Programs/Development/KDL.nix
@@ -1,16 +1,11 @@
 { pkgs, ... }:
 
 let
-  kdl-v1 = pkgs.vscode-utils.buildVscodeExtension {
-    pname = "kdl-v1";
-    version = "1.4.1";
-
-    vscodeExtUniqueId = "kdl-org.kdl-v1";
-    vscodeExtPublisher = "kdl-org";
-    vscodeExtName = "kdl-v1";
-
-    src = pkgs.fetchurl {
-      url = "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/kdl-org/vsextensions/kdl-v1/1.4.1/vspackage";
+  kdl-v1 = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+    mktplcRef = {
+      publisher = "kdl-org";
+      name = "kdl-v1";
+      version = "1.4.1";
       hash = "sha256-xE/Zerw7/ZyI5JYq7PaF5UjMxhINeydkUSz+qR7g4AM=";
     };
   };
