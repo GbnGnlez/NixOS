e72956356e66b922563901fc2ee085f079004da4
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-09-02 09:34:24 -0600

    fix: refactor KDL.nix to build vscode extension using pkgs.vscode-utils

--- Home/Programs/Development/KDL.nix
+++ Home/Programs/Development/KDL.nix
@@ -1,15 +1,21 @@
 { pkgs, ... }:
 
-{
-  programs.vscode = {
-    profiles.default.extensions = with pkgs.vscode-extensions; [
-      kdl-org.kdl-v1
-    ];
+let
+  kdl-v1 = pkgs.vscode-utils.buildVscodeExtension {
+    pname = "kdl-v1";
+    version = "1.4.1";
 
-    profiles.default.userSettings = {
-      "[kdl]" = {
-        "editor.formatOnSave" = true;
-      };
+    src = pkgs.fetchurl {
+      url = "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/kdl-org/vsextensions/kdl-v1/1.4.1/vspackage";
+      hash = pkgs.lib.fakeHash;
     };
+
+    vscodeExtPublisher = "kdl-org";
+    vscodeExtName = "kdl-v1";
   };
+in
+{
+  programs.vscode.profiles.default.extensions = [
+    kdl-v1
+  ];
 }
