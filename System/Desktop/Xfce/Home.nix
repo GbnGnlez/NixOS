77250bdcfb01f4a7f3247bfc3e5174be35a0ba18
GibranNahumGonzalezSantamaria <GGonzalezS1903@alumno.ipn.mx>
2026-09-04 04:22:39 -0600

    feat: add xfwm4 configuration file to Xfce desktop settings

--- System/Desktop/Xfce/Home.nix
+++ System/Desktop/Xfce/Home.nix
@@ -1,5 +1,6 @@
 { ... }:
 
 {
+  xdg.configFile."xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml".source = ./xfwm4.xml;
   xdg.configFile."xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml".source = ./xfce4-panel.xml;
 }
