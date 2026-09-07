062d0c9e7898504015a05d63fce17f85b84c407d
GbnGnlez <GbnGnlez@outlook.com>
2026-09-07 11:02:20 -0600

    fix: correct image path in Stylix module to use relative reference

--- System/Desktop/Stylix.nix
+++ System/Desktop/Stylix.nix
@@ -13,7 +13,7 @@
 
     # stylix.image es obligatorio para inicializar el módulo.
     # Usa una imagen local (.png o .jpg) en la misma carpeta o ruta absoluta en Nix.
-image = /5120x2880.png;
+image = ./5120x2880.png;
 
  
     # Opcional: define un esquema directo para evitar el cálculo automático de colores
