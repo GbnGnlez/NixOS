9399404fb19d97dddadba76b888449eef22e06d1
GbnGnlez <GbnGnlez@outlook.com>
2026-09-07 11:13:38 -0600

    fix: remove mandatory image setting and clean up comments in Stylix module

--- System/Desktop/Stylix.nix
+++ System/Desktop/Stylix.nix
@@ -11,12 +11,11 @@
     enable = true;
     polarity = if DarkTheme then "dark" else "light";
 
-    # stylix.image es obligatorio para inicializar el módulo.
-    # Usa una imagen local (.png o .jpg) en la misma carpeta o ruta absoluta en Nix.
-    image = ./5120x2880.png;
-
-    # Opcional: define un esquema directo para evitar el cálculo automático de colores
-    # base16Scheme = "${pkgs.base16-schemes}/share/themes/breeze.yaml";
+    base16Scheme =
+      if DarkTheme then
+        "${pkgs.base16-schemes}/share/themes/default-dark.yaml"
+      else
+        "${pkgs.base16-schemes}/share/themes/default-light.yaml";
 
     cursor = {
       package = pkgs.bibata-cursors;
