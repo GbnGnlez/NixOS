8e8cdc81198bf2ea252c70c848c8b7389ef819d1
GbnGnlez <GbnGnlez@outlook.com>
2026-09-07 11:35:19 -0600

    fix: remove commented-out configuration in Common.nix

--- Home/Common.nix
+++ Home/Common.nix
@@ -1,54 +1,5 @@
+{ ... }:
+
 {
-  # pkgs,
-  # DarkTheme,
-  # Color,
-  ...
-}:
-
-# let
-# papirusPersonalizado = pkgs.papirus-icon-theme.override {
-# color = Color;
-# };
-# in
-{
-  # home.packages = with pkgs; [
-  # bibata-cursors
-  # inter
-  # gtk3
-  # papirusPersonalizado
-  # jetbrains-mono
-  # glib
-  # ];
-
-  # Configuración del Cursor (Global para GTK y X11)
-  # home.pointerCursor = {
-  # enable = true;
-  # gtk.enable = true;
-  # x11.enable = true;
-  # name = if DarkTheme then "Bibata-Modern-Ice" else "Bibata-Modern-Classic";
-  # package = pkgs.bibata-cursors;
-  # size = 16;
-  # };
-
-  # gtk = {
-  # enable = true;
-
-  # theme = {
-  # name = if DarkTheme then "Breeze-Dark" else "Breeze";
-  # package = pkgs.kdePackages.breeze-gtk;
-  # };
-
-  # iconTheme = {
-  # name = if DarkTheme then "Papirus-Dark" else "Papirus-Light";
-  # package = papirusPersonalizado;
-  # };
-
-  # font = {
-  # name = "Inter";
-  # size = 10;
-  # package = pkgs.inter;
-  # };
-  # };
-
   home.stateVersion = "26.05";
 }
