33f634077ed3449069b7d43230bb014824cf467c
GbnGnlez <GbnGnlez@outlook.com>
2026-09-07 11:30:20 -0600

    fix: remove unused image files and refactor icon theme handling in Stylix module

--- System/Desktop/Stylix.nix
+++ System/Desktop/Stylix.nix
@@ -1,4 +1,3 @@
-# System/Desktop/Stylix.nix
 {
   pkgs,
   DarkTheme,
@@ -7,8 +6,8 @@
 }:
 
 let
-  iconThemeName = if DarkTheme then "Papirus-Dark" else "Papirus-Light";
-  papirusPersonalizado = pkgs.papirus-icon-theme.override {
+  IconThemeName = if DarkTheme then "Papirus-Dark" else "Papirus-Light";
+  Papirus = pkgs.papirus-icon-theme.override {
     color = Color;
   };
 in
@@ -29,22 +28,49 @@ in
       name = if DarkTheme then "Bibata-Modern-Ice" else "Bibata-Modern-Classic";
       size = 24;
     };
+
+    fonts = {
+      sansSerif = {
+        package = pkgs.inter;
+        name = "Inter";
+      };
+
+      serif = {
+        package = pkgs.inter;
+        name = "Inter";
+      };
+
+      # Set a dedicated monospace font to prevent pulling DejaVu Sans Mono
+      monospace = {
+        package = pkgs.nerd-fonts.jetbrains-mono;
+        name = "JetBrainsMono Nerd Font";
+      };
+
+      emoji = {
+        package = pkgs.noto-fonts-color-emoji;
+        name = "Noto Color Emoji";
+      };
+
+      sizes = {
+        desktop = 10;
+        applications = 11;
+        terminal = 11;
+        popups = 10;
+      };
+    };
   };
 
-  # 1. Disponibiliza el paquete de iconos modificado globalmente
   environment.systemPackages = [
-    papirusPersonalizado
+    Papirus
   ];
 
-  # 2. Inyección de Papirus en aplicaciones GTK 3 y GTK 4 a nivel sistema
   environment.etc."xdg/gtk-3.0/settings.ini".text = ''
-    s
-        [Settings]
-        gtk-icon-theme-name=${iconThemeName}
+    [Settings]
+    gtk-icon-theme-name=${IconThemeName}
   '';
 
   environment.etc."xdg/gtk-4.0/settings.ini".text = ''
     [Settings]
-    gtk-icon-theme-name=${iconThemeName}
+    gtk-icon-theme-name=${IconThemeName}
   '';
 }
