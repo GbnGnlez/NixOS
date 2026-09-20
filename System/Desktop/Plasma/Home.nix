5364ddf68654557fd35feaf50836b1904d03d1e2
GbnGnlez <GbnGnlez@outlook.com>
2026-09-20 03:37:44 -0600

    fix: remove Fonts.nix and clean up Plasma configuration

--- System/Desktop/Plasma/Home.nix
+++ System/Desktop/Plasma/Home.nix
@@ -9,24 +9,7 @@
   imports = [
     ./KScreenLocker.nix
     ./Panels.nix
-    ./Fonts.nix
   ];
-
-  # Set resolution to 1280x720 automatically upon graphical session startup
-  # systemd.user.services.set-display-resolution = {
-  # Unit = {
-  # Description = "Set KDE display resolution to 1280x720";
-  # After = [ "graphical-session.target" ];
-  # PartOf = [ "graphical-session.target" ];
-  # };
-  # Install = {
-  # WantedBy = [ "graphical-session.target" ];
-  # };
-  # Service = {
-  # Type = "oneshot";
-  # ExecStart = "${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.eDP-1.mode.1280x720@60";
-  # };
-  # };
 
   programs.plasma = {
     enable = true;
@@ -39,21 +22,6 @@
     configFile."kwinrc"."NightColor"."DayTemperature" = 6500;
     configFile."kwinrc"."NightColor"."NightTemperature" = 3750;
 
-    workspace = {
-      lookAndFeel = if DarkTheme then "org.kde.breezedark.desktop" else "org.kde.breeze.desktop";
-      iconTheme = if DarkTheme then "Papirus-Dark" else "Papirus-Light";
-
-      cursor = {
-        theme = if DarkTheme then "Bibata-Modern-Ice" else "Bibata-Modern-Classic";
-        size = 16;
-      };
-    };
-
-    configFile."kdeglobals"."General"."AccentColor" =
-      if Color == "blue" then "61,174,233" else "233,58,154";
-    configFile."kdeglobals"."General"."LastUsedCustomAccentColor" =
-      if Color == "blue" then "61,174,233" else "233,58,154";
-    configFile."kdeglobals"."Sounds"."Theme" = "freedesktop";
     configFile."ksplashrc"."KSplash"."Theme" = "None";
   };
 }
