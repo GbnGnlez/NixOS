e5b33bcb4b754b80242942003b7a05680194d6b5
GbnGnlez <GbnGnlez@outlook.com>
2026-09-20 03:39:53 -0600

    fix: remove Plasma program configuration and associated settings

--- System/Desktop/Plasma/Home.nix
+++ System/Desktop/Plasma/Home.nix
@@ -11,17 +11,4 @@
     ./Panels.nix
   ];
 
-  programs.plasma = {
-    enable = true;
-    overrideConfig = true;
-
-    # Night Color
-    configFile."kwinrc"."NightColor"."Active" = true;
-    configFile."kwinrc"."NightColor"."Mode" = "Location";
-    configFile."kwinrc"."NightColor"."LocationMode" = "Automatic";
-    configFile."kwinrc"."NightColor"."DayTemperature" = 6500;
-    configFile."kwinrc"."NightColor"."NightTemperature" = 3750;
-
-    configFile."ksplashrc"."KSplash"."Theme" = "None";
-  };
 }
